62d77b14481b9c9c6c230db72bb0672d37f97346
sadiq-1698 <mohammedsadiq1698@gmail.com>
2024-05-04 18:49:10 +0530

    fix: interval available only on intraday

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import SymbolSelector from './symbol-selector';
 import SelectorDivider from './selector-divider';
 import IntervalSelector from './interval-selector';
 import FunctionSelector from './function-selector';
+import { StockContext } from '../utils/contexts/StockContext';
+import { TIME_SERIES_INTRADAY } from '../utils/constants';
 
 function Header() {
+  const { dataQuery } = useContext(StockContext);
+
   return (
     <div className='px-4 flex items-center h-10 shadow-xl absolute top-0 left-0 w-full z-50 bg-white'>
       <div className='header-content flex items-center'>
         <SymbolSelector />
         <SelectorDivider />
-        <IntervalSelector />
+        {dataQuery.function === TIME_SERIES_INTRADAY && <IntervalSelector />}
         <SelectorDivider />
         <FunctionSelector />
       </div>
